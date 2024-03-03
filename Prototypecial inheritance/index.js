da04977e5a66cf809c54b8eb9212755179641a24
rishishal <rishi.kumar1609@gmail.com>
2024-03-03 12:49:33 +0530

    update 3.1

--- Prototypecial inheritance/index.js
+++ Prototypecial inheritance/index.js
@@ -4,7 +4,7 @@ const obj = {
   city: "Delhi",
   arr: [1, 2, 3, 4],
   getintro: function () {
-    console.log(`hello ${this.name} ${this.last}`);
+    console.log(`hello ${this.name} ${this.last} from ${this.city}`);
   },
 };
 
@@ -18,3 +18,13 @@ obj2.__proto__ = obj; // Now obj2 has a value of obj Object
 
 // we can print the value of obj2.city **Notice** we dont have city propety in obj2
 console.log(obj2.city);
+console.log(obj2.getintro());
+
+//Good way of write Prototype
+Function.prototype.myBind = function () {
+  console.log("Hello Bro");
+};
+
+function fun() {
+  //check a Prototype of function now we are own Prototype myBind
+}
